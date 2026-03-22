521ad95fee6a4c368ba199baa03a126ea063c7d6
RPDJF <86334233+RPDJF@users.noreply.github.com>
2026-03-22 16:38:35 +0100

    feat(frontend): implement form directive to allow disable form

--- matcha-frontend/src/app/pages/authentification/authentification.component/authentification.component.ts
+++ matcha-frontend/src/app/pages/authentification/authentification.component/authentification.component.ts
@@ -4,6 +4,7 @@ import { RouterLink } from '@angular/router';
 import { Store } from '@ngxs/store';
 import { ButtonPrimaryDirective } from '../../../core/directives/buttons/button-primary.directive';
 import { ButtonSecondaryDirective } from '../../../core/directives/buttons/button-secondary.directive';
+import { appFormBase } from '../../../core/directives/forms/form-base.directive';
 import { InputPrimaryDirective } from '../../../core/directives/inputs/input-primary.directive';
 import { MarkupReplacerDirective } from '../../../core/directives/markupReplacer/markupReplacer.directive';
 import { MarkupReplacerTemplate } from '../../../core/directives/markupReplacer/markupReplacerTemplate.directive';
@@ -22,6 +23,7 @@ import { AuthLogin } from '../../../core/stores/auth/auth.actions';
     MarkupReplacerTemplate,
     RouterLink,
     ReactiveFormsModule,
+    appFormBase,
   ],
   templateUrl: './authentification.component.html',
   standalone: true,
