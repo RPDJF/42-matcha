27f4bd0f0b63743b363ee32bfae7b939b1853a5a
RPDJF <86334233+RPDJF@users.noreply.github.com>
2026-04-11 15:37:34 +0200

    fix(user presence): app-avatar now handles user presence

--- matcha-frontend/src/app/components/avatar/avatar.component.ts
+++ matcha-frontend/src/app/components/avatar/avatar.component.ts
@@ -1,21 +1,25 @@
-import { Component, computed, contentChild, inject, input } from '@angular/core';
+import { Component, computed, inject, input, OnDestroy, OnInit, viewChild } from '@angular/core';
 import { Store } from '@ngxs/store';
 import { PublicUser } from '../../core/stores/user/user.state.types';
 import { UserPresenceState } from '../../core/stores/userPresence/userPresence.state';
+import {
+  UserPresenceAddTrackedUsers,
+  UserPresenceRemoveTrackedUsers,
+} from './../../core/stores/userPresence/userPresence.actions';
 
 @Component({
   selector: 'app-avatar',
   imports: [],
   templateUrl: './avatar.component.html',
 })
-export class AvatarComponent {
+export class AvatarComponent implements OnInit, OnDestroy {
   readonly #store = inject(Store);
   readonly user = input.required<PublicUser>();
 
   readonly size = input<'md' | 'sm'>('md');
   readonly displayBadge = input<boolean>(true);
 
-  readonly badgeElement = contentChild('badge');
+  readonly presenceBadgeRef = viewChild('presenceBadge');
 
   readonly #presenceSignal = computed(() => {
     if (!this.displayBadge()) {
@@ -28,4 +32,18 @@ export class AvatarComponent {
   });
 
   readonly presence = computed(() => this.#presenceSignal()?.());
+
+  ngOnInit(): void {
+    requestAnimationFrame(() => {
+      if (this.presenceBadgeRef()) {
+        this.#store.dispatch(new UserPresenceAddTrackedUsers([this.user().userUUID]));
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.presenceBadgeRef()) {
+      this.#store.dispatch(new UserPresenceRemoveTrackedUsers([this.user().userUUID]));
+    }
+  }
 }
